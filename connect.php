ab36aa15b460c9a758d6b12cba93500b99f97854
Kavita <kavitas2411@gmail.com>
2023-04-04 01:15:02 -0400

    changes

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@
     $database = 'kidsgames';
 
     $connection = new mysqli($hostname, $username, $password);
+
     mysqli_select_db($connection, $database);
     date_default_timezone_set("America/New_York");
 ?>
