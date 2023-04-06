9fbec4f492c5f806524d8194ec8b459f47144a19
amarsimran <Simran98774@gmail.com>
2023-04-06 12:44:34 -0400

    12:44 PM 06-04-2023
    level5.php
    level6.php

--- level5.php
+++ level5.php
@@ -1,64 +1,69 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Kids Game|level5</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-</head>
-
-<body>
-
-
-	<h1 class="text-center">Level 5</h1>
-    
-</body>
 <?php
-session_start();
 
-// generate random letters
-$letters = array();
-for ($i = 0; $i < 6; $i++) {
-    $letters[] = chr(rand(97, 122)); // generate random lowercase letters
-}
+function checkSameSpec($str) {
+    $firstLetter = $str[0];
+    $lastLetter = $str[strlen($str)-1];
+    $letters = "abcdefghijklmnopqrstuvwxyz";
+    $isSameSpec = true;
 
-// store the letters in session
-$_SESSION['letters'] = $letters;
-
-// check if the form has been submitted
-if (isset($_POST['submit'])) {
-    $first_letter = $_POST['first_letter'];
-    $last_letter = $_POST['last_letter'];
-    
-    // check if the user entered the correct letters
-    if ($first_letter == $letters[0] && $last_letter == $letters[9]) {
-
-        // show success message with button to go to next level or try again
-        echo "<p>Congratulations, you identified the first and last letters!</p>";
-        echo "<button><a href='level6.php'>Go to Next Level</a></button>";
-        echo "<button><a href='level5.php'>Try Again this Level</a></button>";
-        echo "<button><a href='index.php'>Login</a></button>";
-        echo "<button><a href='signUp.php'>Submit</a></button>";
-    } else {
-
-        // show error message with button to try again
-        echo "<p>Sorry, you did not identify the correct letters.</p>";
-        echo "<button><a href='level5.php'>Try Again this Level</a></button>";
-        echo "<button><a href='index.php'>Login</a></button>";
-        echo "<button><a href='signUp.php'>Submit</a></button>";
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] != $firstLetter && $str[$i] != $lastLetter) {
+            $isSameSpec = false;
+            break;
+        }
     }
-} else {
-	
-    // show the form to the user
-    echo "<p>Identify the first and last letters from the set of letters:</p>";
-    echo "<form method='post' action='level5.php'>";
-    echo "<label for='first_letter'>First Letter:</label>";
-    echo "<input type='text' name='first_letter' maxlength='1' required>";
-    echo "<label for='last_letter'>Last Letter:</label>";
-    echo "<input type='text' name='last_letter' maxlength='1' required>";
-    echo "<button type='submit' name='submit'>Submit</button>";
-    echo "</form>";
-    echo "<button><a href='index.php'>Login</a></button>";
-   
+
+    if (strpos($letters, $firstLetter) !== false && strpos($letters, $lastLetter) !== false && $isSameSpec) {
+        return "You Won";
+    } else {
+        return "Try again";
+    }
 }
+
+// Generate a random string of length 10 containing only characters
+$characters = 'abcdefghijklmnopqrstuvwxyz';
+$randomString = substr(str_shuffle($characters), 0, 10);
+
+// Pick a random index to identify the first and last letters
+$firstLetterIndex = rand(0, strlen($randomString)-1);
+$lastLetterIndex = rand(0, strlen($randomString)-1);
+while ($firstLetterIndex == $lastLetterIndex) {
+    $lastLetterIndex = rand(0, strlen($randomString)-1);
+}
+
+// Extract the first and last letters
+$firstLetter = $randomString[$firstLetterIndex];
+$lastLetter = $randomString[$lastLetterIndex];
+
+// Output the question
+echo "Identify the first and last letter of this string: " . $randomString . "<br>";
+
+// Check the user's answer if provided
+if (isset($_GET["inputResult1"]) && isset($_GET["inputResult2"])) {
+    $userAnswer = $_GET["inputResult1"] . $_GET["inputResult2"];
+    $result = checkSameSpec($userAnswer);
+    echo "Result: " . $result . "<br>";
+    if ($result == "You Won") {
+        echo "Congratulations! You have successfully completed this level. ";
+        echo "<a href='level6.php'>Go to Next Level 6</a>";
+      
+    }else{
+        echo"Oops, your answers are incorrect!!.";
+    }
+} 
+
+
+
 ?>
+
+
+<h1 class="text-center">Level 5</h1>
+    
+<form method="get">
+    <label for="inputResult1">First letter: </label>
+    <input type="text" id="inputResult1" name="inputResult1"><br>
+    <label for="inputResult2">Last letter: </label>
+    <input type="text" id="inputResult2" name="inputResult2"><br>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+</form>
--- level6.php
+++ level6.php
@@ -1,66 +1,46 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Kids Game|level6</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-</head>
-
-<body>
-
-
-	<h2 class="text-center">Level 6</h2>
-</body>
-
 <?php
-session_start();
 
-// generate random numbers
-$numbers = array();
-for ($i = 0; $i < 6; $i++) {
-    $numbers[] = rand(0, 100);
-}
-
-// store the numbers in session
-$_SESSION['numbers'] = $numbers;
-
-// check if the form has been submitted
-if (isset($_POST['submit'])) {
-    $min_number = $_POST['min_number'];
-    $max_number = $_POST['max_number'];
-    
-    // check if the user entered the correct numbers
-
-    if ($min_number == min($numbers) && $max_number == max($numbers)) {
-      
-        echo "<p>Congratulations, you identified the minimum and maximum numbers!</p>";
-        echo "<button><a href='level6.php'>Play Again</a></button>";
-        echo "<button><a href='Index.php'>Login</a></button>";
-        echo "<button><a href='signUp.php'>Sign Out</a></button>";
+function checkMinMax($arr) {
+    $min = min($arr);
+    $max = max($arr);
+    if ($min == 1 && $max == 100) {
+        return "You Won";
     } else {
-
-        // show error message with button to try again or sign out
-        echo "<p>Sorry, your answer is incorrect. Please try again.</p>";
-        echo "<button><a href='level6.php'>Try Again</a></button>";
-        echo "<button><a href='signUp.php'>Sign Out</a></button>";
+        return "Try again";
     }
-} else {
-	
-    // show the form to the user
-    echo "<p>Identify the minimum and maximum numbers from the following numbers:</p>";
-    echo "<p>" . implode(", ", $numbers) . "</p>";
-    echo "<form method='POST'>";
-    echo "<label for='min_number'>Minimum Number:</label>";
-    echo "<input type='number' id='min_number' name='min_number' min='0' max='100' required>";
-    echo "<br>";
-    echo "<label for='max_number'>Maximum Number:</label>";
-    echo "<input type='number' id='max_number' name='max_number' min='0' max='100' required>";
-    echo "<br>";
-    echo "<input type='submit' name='submit' value='Submit'>";
-    echo "</form>";
-    // show buttons to sign out or stop the session
-   
-    echo "<button><a href='index.php'>Login</a></button>";
 }
+
+// Generate an array of 10 random integers between 1 and 100
+$numbers = array();
+for ($i = 0; $i < 10; $i++) {
+    $numbers[] = rand(1, 100);
+}
+
+// Output the question
+echo "Identify the minimum and maximum number in this array: " . implode(", ", $numbers) . "<br>";
+
+// Check the user's answer if provided
+if (isset($_GET["inputResult1"]) && isset($_GET["inputResult2"])) {
+    $userAnswer = array(intval($_GET["inputResult1"]), intval($_GET["inputResult2"]));
+    $result = checkMinMax($userAnswer);
+    echo "Result: " . $result . "<br>";
+    if ($result == "You Won") {
+        echo "Congratulations! You have successfully completed this game. ";
+        echo "<a href='history.php'>Go to History Page</a>";
+    } else {
+        echo "Oops, your answers are incorrect!";
+    }
+}
+
 ?>
+
+<h1 class="text-center">Level 6</h1>
+
+<form method="get">
+    <label for="inputResult1">Minimum number: </label>
+    <input type="number" id="inputResult1" name="inputResult1"><br>
+    <label for="inputResult2">Maximum number: </label>
+    <input type="number" id="inputResult2" name="inputResult2"><br>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+</form>
