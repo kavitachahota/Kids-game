d9f2f859e7653cfa20ab747fbc17a7542784838b
thevarungrovers <grover82vinni@gmail.com>
2023-04-09 14:13:09 -0400

    09/04/2023 14:12
    modal feature added

--- level3/index.php
+++ level3/index.php
@@ -7,16 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
     <script type="text/javascript" src="main.js"></script>
-
-    <style> 
-        #txtHint{color: red}
-        .heading-block{
-            margin-top: 10px ;
-        } 
-        #submit-btn{
-            display: none;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
@@ -53,9 +44,9 @@
             </p>
         </div>
         <div class="bts-container">
-            <a href="../level4">
-                <button type="button" class="btn btn-primary" id="submit-btn">Submit</button>
-            </a>    
+            <button type="button" id="submit-btn" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Submit
+            </button>      
             <input type="reset" class="btn btn-primary" value="Try again">
             <a href="../logout.php">
                 <button type="button" class="btn btn-primary">Stop the session</button>
@@ -63,6 +54,37 @@
             </form>
         </div>
     </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">
+            Congrats
+        </h1>
+        <img src="https://cdn-icons-png.flaticon.com/512/864/864837.png" id="congrats-img" alt="">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        You Have Successfully Completed the Challenge..
+      </div>
+      <div class="modal-footer">
+        <a href="index.php">
+            <button type="button" class="btn btn-primary">Try Again</button>
+        </a>
+        <a href="../logout.php">
+            <button type="button" class="btn btn-primary">Stop the Session</button>
+        </a>
+        <a href="../level4">
+            <button type="button" class="btn btn-primary">Go to Next Level</button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -5,8 +5,10 @@ session_start();
     <p class="container">
         You have successfully logout.
         <br>
-        Please <a href="index.php">Login</a> again to continue again.
+        Please <a href="index.php">Login</a> again to continue.
     </p>
  END;
+
+ #$_SESSION[""]
  session_destroy();
 ?>
