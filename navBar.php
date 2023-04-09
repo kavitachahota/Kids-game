368c717bb0799315467b6c9ef5a00ca293df8631
thevarungrovers <grover82vinni@gmail.com>
2023-04-09 14:14:13 -0400

    09/04/2023 14:14
    navbar updated

--- navBar.php
+++ navBar.php
@@ -15,7 +15,7 @@
 					<a class="nav-link active" aria-current="page" href="index.php">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="history.php">History</a>
+					<a class="nav-link" href="../history.php">History</a>
 				</li>
 				<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
