624d411deed391c381d7e361c297af2f3101f270
thevarungrovers <grover82vinni@gmail.com>
2023-04-10 12:28:22 -0400

    10/04/2023 12:30
    level 5 and 6 updated with same frontend as others

--- level5.php
+++ level5.php
@@ -1,23 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Level 5 | Kids Game</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+	
+<?php
+
+include "navBar.php";
+
+
+if (!isset($_SESSION['username'])) {
+die('ERROR: Please, <a href="index.php">Login</a> first to be able to access this page.');
+}
+
+?>
+
+<h1 class="text-center">Level 5</h1>
+
+<div class="wrapper container">
+
+
+
 <?php
 
 function checkSameSpec($str) {
-    $firstLetter = $str[0];
-    $lastLetter = $str[strlen($str)-1];
-    $letters = "abcdefghijklmnopqrstuvwxyz";
-    $isSameSpec = true;
+$firstLetter = $str[0];
+$lastLetter = $str[strlen($str)-1];
+$letters = "abcdefghijklmnopqrstuvwxyz";
+$isSameSpec = true;
 
-    for ($i = 0; $i < strlen($str); $i++) {
-        if ($str[$i] != $firstLetter && $str[$i] != $lastLetter) {
-            $isSameSpec = false;
-            break;
-        }
-    }
+for ($i = 0; $i < strlen($str); $i++) {
+if ($str[$i] != $firstLetter && $str[$i] != $lastLetter) {
+$isSameSpec = false;
+break;
+}
+}
 
-    if (strpos($letters, $firstLetter) !== false && strpos($letters, $lastLetter) !== false && $isSameSpec) {
-        return "You Won";
-    } else {
-        return "Try again";
-    }
+if (strpos($letters, $firstLetter) !== false && strpos($letters, $lastLetter) !== false && $isSameSpec) {
+return "You Won";
+} else {
+return "Try again";
+}
 }
 
 // Generate a random string of length 10 containing only characters
@@ -28,7 +55,7 @@ $randomString = substr(str_shuffle($characters), 0, 10);
 $firstLetterIndex = rand(0, strlen($randomString)-1);
 $lastLetterIndex = rand(0, strlen($randomString)-1);
 while ($firstLetterIndex == $lastLetterIndex) {
-    $lastLetterIndex = rand(0, strlen($randomString)-1);
+$lastLetterIndex = rand(0, strlen($randomString)-1);
 }
 
 // Extract the first and last letters
@@ -36,34 +63,49 @@ $firstLetter = $randomString[$firstLetterIndex];
 $lastLetter = $randomString[$lastLetterIndex];
 
 // Output the question
-echo "Identify the first and last letter of this string: " . $randomString . "<br>";
+echo "Identify the first and last letter of this string: <h2>" . $randomString . "</h2><br>";
 
 // Check the user's answer if provided
 if (isset($_GET["inputResult1"]) && isset($_GET["inputResult2"])) {
-    $userAnswer = $_GET["inputResult1"] . $_GET["inputResult2"];
-    $result = checkSameSpec($userAnswer);
-    echo "Result: " . $result . "<br>";
-    if ($result == "You Won") {
-        echo "Congratulations! You have successfully completed this level. ";
-        echo "<a href='level6.php'>Go to Next Level 6</a>";
-      
-    }else{
-        echo"Oops, your answers are incorrect!!.";
-    }
+$userAnswer = $_GET["inputResult1"] . $_GET["inputResult2"];
+$result = checkSameSpec($userAnswer);
+echo "Result: " . $result . "<br>";
+if ($result == "You Won") {
+echo "Congratulations! You have successfully completed this level. ";
+echo "<a href='level6.php'>Go to Next Level 6</a>";
+
+}else{
+echo"Oops, your answers are incorrect!!.";
+}
 } 
-
-
-
 ?>
 
+<br>
 
-<h1 class="text-center">Level 5</h1>
-    
+
+
 <form method="get">
-    <label for="inputResult1">First letter: </label>
-    <input type="text" id="inputResult1" name="inputResult1"><br>
-    <label for="inputResult2">Last letter: </label>
-    <input type="text" id="inputResult2" name="inputResult2"><br>
-    <input type="submit" value="Submit">
-    <input type="reset" value="Reset">
+<div class="mb-3">
+<label for="inputResult1" class="form-label">First letter: </label>
+<input type="text" class="form-control" id="inputResult1" 
+name="inputResult1">
+</div>
+<div class="mb-3">
+<label for="inputResult2" class="form-label">Last letter: </label>
+<input type="text" class="form-control" id="inputResult2" 
+name="inputResult2">
+</div>
+<!-- 
+<label for="inputResult1">First letter: </label>
+<input type="text" id="inputResult1" name="inputResult1"><br> -->
+<!-- <label for="inputResult2">Last letter: </label>
+<input type="text" id="inputResult2" name="inputResult2"><br> -->
+<input type="submit"  class="btn btn-primary" value="Submit">
+<input type="reset"  class="btn btn-primary" value="Reset">
 </form>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
--- level6.php
+++ level6.php
@@ -1,46 +1,84 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Level 6 | Kids Game</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+	
+        
+    <?php
+        include "navBar.php";
 
-function checkMinMax($arr) {
-    $min = min($arr);
-    $max = max($arr);
-    if ($min == 1 && $max == 100) {
-        return "You Won";
-    } else {
-        return "Try again";
-    }
-}
 
-// Generate an array of 10 random integers between 1 and 100
-$numbers = array();
-for ($i = 0; $i < 10; $i++) {
-    $numbers[] = rand(1, 100);
-}
+        if (!isset($_SESSION['username'])) {
+            die('ERROR: Please, <a href="index.php">Login</a> first to be able to access this page.');
+        }
+    ?>
 
-// Output the question
-echo "Identify the minimum and maximum number in this array: " . implode(", ", $numbers) . "<br>";
+    <h1 class="text-center">Level 6</h1>
 
-// Check the user's answer if provided
-if (isset($_GET["inputResult1"]) && isset($_GET["inputResult2"])) {
-    $userAnswer = array(intval($_GET["inputResult1"]), intval($_GET["inputResult2"]));
-    $result = checkMinMax($userAnswer);
-    echo "Result: " . $result . "<br>";
-    if ($result == "You Won") {
-        echo "Congratulations! You have successfully completed this game. ";
-        echo "<a href='history.php'>Go to History Page</a>";
-    } else {
-        echo "Oops, your answers are incorrect!";
-    }
-}
+    <div class="wrapper container">
 
-?>
+        <?php
 
-<h1 class="text-center">Level 6</h1>
+            function checkMinMax($arr) {
+                $min = min($arr);
+                $max = max($arr);
+                if ($min == 1 && $max == 100) {
+                    return "You Won";
+                } else {
+                    return "Try again";
+                }
+            }
 
-<form method="get">
-    <label for="inputResult1">Minimum number: </label>
-    <input type="number" id="inputResult1" name="inputResult1"><br>
-    <label for="inputResult2">Maximum number: </label>
-    <input type="number" id="inputResult2" name="inputResult2"><br>
-    <input type="submit" value="Submit">
-    <input type="reset" value="Reset">
-</form>
+            // Generate an array of 10 random integers between 1 and 100
+            $numbers = array();
+            for ($i = 0; $i < 10; $i++) {
+                $numbers[] = rand(1, 100);
+            }
+
+            // Output the question
+            echo "Identify the minimum and maximum number in this array: <h2>" . implode(", ", $numbers) . "</h2><br>";
+
+            // Check the user's answer if provided
+            if (isset($_GET["inputResult1"]) && isset($_GET["inputResult2"])) {
+                $userAnswer = array(intval($_GET["inputResult1"]), intval($_GET["inputResult2"]));
+                $result = checkMinMax($userAnswer);
+                echo "Result: " . $result . "<br>";
+                if ($result == "You Won") {
+                    echo "Congratulations! You have successfully completed this game. ";
+                    echo "<a href='history.php'>Go to History Page</a>";
+                } else {
+                    echo "Oops, your answers are incorrect!";
+                }
+            }
+
+        ?>
+
+        <form method="get">
+            <div class="mb-3">
+                <label for="inputResult1" class="form-label">Minimum number: </label>
+                <input type="number" class="form-control" id="inputResult1" 
+                name="inputResult1">
+            </div>
+            <div class="mb-3">
+                <label for="inputResult2" class="form-label">Maximum number: </label>
+                <input type="number" class="form-control" id="inputResult2" 
+                name="inputResult2">
+            </div>
+
+            <!-- <label for="inputResult1">Minimum number: </label>
+            <input type="number" id="inputResult1" name="inputResult1"><br>
+            <label for="inputResult2">Maximum number: </label>
+            <input type="number" id="inputResult2" name="inputResult2"><br> -->
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <input type="reset" class="btn btn-primary" value="Reset">
+        </form>
+    </div>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
